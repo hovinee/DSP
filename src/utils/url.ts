d5030a5f61400af7506fb13dc0349be147aa61cb
hobinyoo <97422131+hobinyoo@users.noreply.github.com>
2023-12-18 12:13:15 +0900

    url 변경

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,6 +1,3 @@
-export const getBaseUrl =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3000'
-    : `${process.env.NEXT_PUBLIC_VERCEL_URL}`
+export const getBaseUrl = 'https://www.labkid-industry.com'
 
 export const cfWorkerUrl = 'https://futureverse-worker.team3.workers.dev'
