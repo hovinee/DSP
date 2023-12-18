2ec5578fce4992d7ed6af217110b81b237f12db3
hobinyoo <97422131+hobinyoo@users.noreply.github.com>
2023-12-18 12:27:52 +0900

    getBaseUrl 수정

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,6 +1,6 @@
 export const getBaseUrl =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
-    : `${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : 'https://www.labkid-industry.com/'
 
 export const cfWorkerUrl = 'https://futureverse-worker.team3.workers.dev'
