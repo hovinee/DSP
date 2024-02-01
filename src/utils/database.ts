e03365ce47842b429ab9b795867459b346042aeb
hobinyoo <97422131+hobinyoo@users.noreply.github.com>
2024-02-01 14:46:06 +0900

    fix:db 로직 수정

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -14,7 +14,7 @@ class Database {
   async connect() {
     try {
       await mongoose.connect(
-        'mongodb+srv://admin:rDTVFFQOvu9Oa1OM@cluster0.viw3kqj.mongodb.net/FutureVerse?retryWrites=true&w=majority',
+        'mongodb+srv://admin:rDTVFFQOvu9Oa1OM@cluster0.viw3kqj.mongodb.net/LabkidIndustry?retryWrites=true&w=majority',
       )
       console.log('Database connected successfully.')
     } catch (error) {
